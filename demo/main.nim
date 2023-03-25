476fb6b3b0bf6da3caae833cfca89b4428299773
Pixeller <Angluca@users.noreply.github.com>
2023-03-26 01:07:24 +0800

    'mu.(ctx' to 'ctx.('

--- demo/main.nim
+++ demo/main.nim
@@ -17,99 +17,99 @@ proc test_window(ctx: PContext) =
   var
     win: ptr Container
     buf: string
-  if mu.begin_window(ctx, "demo win", mu.rect(40,40,300,450))!=0:
+  if ctx.begin_window("demo win", mu.rect(40,40,300,450))!=0:
     win = get_current_container(ctx)
     win.rect.w = mu.max(win.rect.w, 240)
     win.rect.h = mu.max(win.rect.h, 300)
 
-    if mu.header(ctx, "win info")!=0:
+    if ctx.header("win info")!=0:
       win = get_current_container(ctx)
       let v {.global.} = [54.cint, -1]
-      mu.layout_row(ctx, 2,v[0].addr, 0)
-      mu.label(ctx, "position:")
-      buf = $win.rect.x & "," & $win.rect.y; mu.label(ctx, buf.cstring)
-      mu.label(ctx, "size:")
-      buf = $win.rect.w & "," & $win.rect.h; mu.label(ctx, buf.cstring)
+      ctx.layout_row(2,v[0].addr, 0)
+      ctx.label("position:")
+      buf = $win.rect.x & "," & $win.rect.y; ctx.label(buf.cstring)
+      ctx.label("size:")
+      buf = $win.rect.w & "," & $win.rect.h; ctx.label(buf.cstring)
 
-    if mu.header_ex(ctx, "test buttons", OPT_EXPANDED)!=0:
+    if ctx.header_ex("test buttons", OPT_EXPANDED)!=0:
       let v {.global.} = [86.cint, -110, -1]
-      mu.layout_row(ctx, 3, v[0].addr, 0)
-      mu.label(ctx, "test btn1:")
-      if mu.button(ctx, "btn1:")!=0: wlog("pres btn1")
-      if mu.button(ctx, "btn2:")!=0: wlog("pres btn2")
-      mu.label(ctx, "test btn2:")
-      if mu.button(ctx, "btn3:")!=0: wlog("pres btn3")
-      if mu.button(ctx, "popup:")!=0: mu.open_popup(ctx, "test popup")
-      if mu.begin_popup(ctx, "test popup")!=0:
-        discard mu.button(ctx, "hello")
-        discard mu.button(ctx, "world")
-        mu.end_popup(ctx)
+      ctx.layout_row(3, v[0].addr, 0)
+      ctx.label("test btn1:")
+      if ctx.button("btn1:")!=0: wlog("pres btn1")
+      if ctx.button("btn2:")!=0: wlog("pres btn2")
+      ctx.label("test btn2:")
+      if ctx.button("btn3:")!=0: wlog("pres btn3")
+      if ctx.button("popup:")!=0: ctx.open_popup("test popup")
+      if ctx.begin_popup("test popup")!=0:
+        discard ctx.button("hello")
+        discard ctx.button("world")
+        ctx.end_popup()
 
-    if mu.header_ex(ctx, "tree and text", OPT_EXPANDED)!=0:
+    if ctx.header_ex("tree and text", OPT_EXPANDED)!=0:
       let v {.global.} = [140.cint, -1]
-      mu.layout_row(ctx, 2, v[0].addr, 0)
-      mu.layout_begin_column(ctx)
-      if mu.begin_treenode(ctx, "test 1")!=0:
-        if mu.begin_treenode(ctx, "test 1a")!=0:
-          mu.label(ctx, "hello")
-          mu.label(ctx, "tree!")
-          mu.end_treenode(ctx)
-        if mu.begin_treenode(ctx, "test 1b")!=0:
-          if mu.button(ctx, "tbtn1:")!=0: wlog("pres tbtn1")
-          if mu.button(ctx, "tbtn2:")!=0: wlog("pres tbtn2")
-          mu.end_treenode(ctx)
-        mu.end_treenode(ctx)
+      ctx.layout_row(2, v[0].addr, 0)
+      ctx.layout_begin_column()
+      if ctx.begin_treenode("test 1")!=0:
+        if ctx.begin_treenode("test 1a")!=0:
+          ctx.label("hello")
+          ctx.label("tree!")
+          ctx.end_treenode()
+        if ctx.begin_treenode("test 1b")!=0:
+          if ctx.button("tbtn1:")!=0: wlog("pres tbtn1")
+          if ctx.button("tbtn2:")!=0: wlog("pres tbtn2")
+          ctx.end_treenode()
+        ctx.end_treenode()
 
-      if mu.begin_treenode(ctx, "test 2")!=0:
+      if ctx.begin_treenode("test 2")!=0:
         let v {.global.} = [54.cint, 54]
-        mu.layout_row(ctx, 2, v[0].addr, 0)
-        if mu.button(ctx, "tbtn3:")!=0: wlog("pres tbtn3")
-        if mu.button(ctx, "tbtn4:")!=0: wlog("pres tbtn4")
-        if mu.button(ctx, "tbtn5:")!=0: wlog("pres tbtn5")
-        if mu.button(ctx, "tbtn6:")!=0: wlog("pres tbtn6")
-        mu.end_treenode(ctx)
+        ctx.layout_row(2, v[0].addr, 0)
+        if ctx.button("tbtn3:")!=0: wlog("pres tbtn3")
+        if ctx.button("tbtn4:")!=0: wlog("pres tbtn4")
+        if ctx.button("tbtn5:")!=0: wlog("pres tbtn5")
+        if ctx.button("tbtn6:")!=0: wlog("pres tbtn6")
+        ctx.end_treenode()
 
-      if mu.begin_treenode(ctx, "test 3")!=0:
+      if ctx.begin_treenode("test 3")!=0:
         let chks {.global.} = [1.cint, 0, 1]
-        discard mu.checkbox(ctx, "chkbox1", chks[0].addr)
-        discard mu.checkbox(ctx, "chkbox1", chks[1].addr)
-        discard mu.checkbox(ctx, "chkbox1", chks[2].addr)
-        mu.end_treenode(ctx)
-      mu.layout_end_column(ctx)
+        discard ctx.checkbox("chkbox1", chks[0].addr)
+        discard ctx.checkbox("chkbox1", chks[1].addr)
+        discard ctx.checkbox("chkbox1", chks[2].addr)
+        ctx.end_treenode()
+      ctx.layout_end_column()
 
-      mu.layout_begin_column(ctx)
+      ctx.layout_begin_column()
       let v2 {.global.} = [-1.cint]
-      mu.layout_row(ctx, 1, v2[0].addr, 0)
-      mu.text(ctx, "Lorem ipsum dolor sit amet, consectetur adipiscing \nelit. Maecenas lacinia, sem eu lacinia molestie, mi risus faucibus \nipsum, eu varius magna felis a nulla.")
-      mu.layout_end_column(ctx)
+      ctx.layout_row(1, v2[0].addr, 0)
+      ctx.text("Lorem ipsum dolor sit amet, consectetur adipiscing \nelit. Maecenas lacinia, sem eu lacinia molestie, mi risus faucibus \nipsum, eu varius magna felis a nulla.")
+      ctx.layout_end_column()
 
-    if mu.header_ex(ctx, "bg color", OPT_EXPANDED)!=0:
+    if ctx.header_ex("bg color", OPT_EXPANDED)!=0:
       let v {.global.} = [-78.cint, -1]
-      mu.layout_row(ctx, 2, v[0].addr, 74)
+      ctx.layout_row(2, v[0].addr, 74)
 
-      mu.layout_begin_column(ctx)
+      ctx.layout_begin_column()
       let v2 {.global.} = [46.cint, -1]
-      mu.layout_row(ctx, 2, v2[0].addr, 0)
-      mu.label(ctx, "red:"); discard mu.slider(ctx, bg[0].addr, 0.Real, 255.Real)
-      mu.label(ctx, "green:"); discard mu.slider(ctx, bg[1].addr, 0.Real, 255.Real)
-      mu.label(ctx, "blue:"); discard mu.slider(ctx, bg[2].addr, 0.Real, 255.Real)
-      mu.layout_end_column(ctx)
+      ctx.layout_row(2, v2[0].addr, 0)
+      ctx.label("red:"); discard ctx.slider(bg[0].addr, 0.Real, 255.Real)
+      ctx.label("green:"); discard ctx.slider(bg[1].addr, 0.Real, 255.Real)
+      ctx.label("blue:"); discard ctx.slider(bg[2].addr, 0.Real, 255.Real)
+      ctx.layout_end_column()
 
-      var r = mu.layout_next(ctx)
-      mu.draw_rect(ctx, r, mu.color(bg[0].cint, bg[1].cint, bg[2].cint, 255.cint))
+      var r = ctx.layout_next()
+      ctx.draw_rect(r, mu.color(bg[0].cint, bg[1].cint, bg[2].cint, 255.cint))
       var buf = $bg[0].int & "," & $bg[1].int & "," & $bg[2].int
-      mu.draw_control_text(ctx, buf.cstring, r, COLOR_TEXT, OPT_ALIGNCENTER)
-    mu.end_window(ctx)
+      ctx.draw_control_text(buf.cstring, r, COLOR_TEXT, OPT_ALIGNCENTER)
+    ctx.end_window()
 
 proc log_window(ctx: PContext) =
-  if mu.begin_window(ctx, "log win", mu.rect(350, 40, 300, 200))!=0:
+  if ctx.begin_window("log win", mu.rect(350, 40, 300, 200))!=0:
     let v {.global.} = [-1.cint]
-    mu.layout_row(ctx, 1, v[0].addr, -25)
-    mu.begin_panel(ctx, "log output")
-    var panel = mu.get_current_container(ctx)
-    mu.layout_row(ctx, 1, v[0].addr, -1)
-    mu.text(ctx, logbuf.cstring)
-    mu.end_panel(ctx)
+    ctx.layout_row(1, v[0].addr, -25)
+    ctx.begin_panel("log output")
+    var panel = ctx.get_current_container()
+    ctx.layout_row(1, v[0].addr, -1)
+    ctx.text(logbuf.cstring)
+    ctx.end_panel()
     if logbuf_updated:
       panel.scroll.y = panel.content_size.y
       logbuf_updated = false
@@ -119,48 +119,48 @@ proc log_window(ctx: PContext) =
       buf: cstring = cast[cstring](buf0.addr)
       bsub = false
       v2 {.global.} = [-70.cint, -1]
-    mu.layout_row(ctx, 2, v2[0].addr, 0)
-    if (mu.textbox(ctx, buf, buf0.high) and RES_SUBMIT) != 0:
-      mu.set_focus(ctx, ctx.last_id)
+    ctx.layout_row(2, v2[0].addr, 0)
+    if (ctx.textbox(buf, buf0.high) and RES_SUBMIT) != 0:
+      ctx.set_focus(ctx.last_id)
       bsub = true
-    if mu.button(ctx, "submit") != 0: bsub = true
+    if ctx.button("submit") != 0: bsub = true
     if bsub:
       wlog(buf)
       zeroMem(buf, buf.len)
 
-    mu.end_window(ctx)
+    ctx.end_window()
 
 proc uint8_slider(ctx: PContext, val: ptr uint8, low:int, high:int): int  =
   var tmp {.global.}: Real
-  mu.push_id(ctx, val.addr, sizeof(val).cint)
+  ctx.push_id(val.addr, sizeof(val).cint)
   tmp = val[].Real
-  result = mu.slider_ex(ctx, tmp.addr, low.Real, high.Real, 0.Real, "%.0f", OPT_ALIGNCENTER)
+  result = ctx.slider_ex(tmp.addr, low.Real, high.Real, 0.Real, "%.0f", OPT_ALIGNCENTER)
   val[] = tmp.uint8
-  mu.pop_id(ctx)
+  ctx.pop_id()
 
 proc style_window(ctx: PContext) =
   let colors {.global.} = [ "text:", "border:", "windowbg:", "titlebg:", "titletext:", "panelbg:", "button:", "buttonhover:", "buttonfocus:", "base:", "basehover:", "basefocus:", "scrollbase:", "scrollthumb:" ]
-  if mu.begin_window(ctx, "style editor", mu.rect(350, 250, 300, 240))!=0:
+  if ctx.begin_window("style editor", mu.rect(350, 250, 300, 240))!=0:
     var
-      sw = (mu.get_current_container(ctx).body.w.float * 0.14).cint
+      sw = (ctx.get_current_container().body.w.float * 0.14).cint
       v = [80.cint, sw, sw, sw, sw, -1]
-    mu.layout_row(ctx, 6, v[0].addr, 0)
+    ctx.layout_row(6, v[0].addr, 0)
     for i, v in colors:
-      mu.label(ctx, v.cstring)
+      ctx.label(v.cstring)
       discard uint8_slider(ctx, ctx.style.colors[i].r.addr, 0, 255)
       discard uint8_slider(ctx, ctx.style.colors[i].g.addr, 0, 255)
       discard uint8_slider(ctx, ctx.style.colors[i].b.addr, 0, 255)
       discard uint8_slider(ctx, ctx.style.colors[i].a.addr, 0, 255)
-      mu.draw_rect(ctx, mu.layout_next(ctx), ctx.style.colors[i])
-    mu.end_window(ctx)
+      ctx.draw_rect(ctx.layout_next(), ctx.style.colors[i])
+    ctx.end_window()
 
 
 proc process_frame(ctx: PContext) =
-  mu.begin(ctx)
+  ctx.begin()
   style_window(ctx)
   log_window(ctx)
   test_window(ctx)
-  mu.end(ctx)
+  ctx.end()
 
 const button_map = {
   BUTTON_LEFT   and 0xff : MOUSE_LEFT,
@@ -192,7 +192,7 @@ proc main =
   r_init()
 
   var ctx: PContext = cast[PContext](alloc0(sizeof(mu.Context)))
-  mu.init(ctx)
+  ctx.init()
   ctx.text_width = text_width
   ctx.text_height = text_height
 
@@ -205,30 +205,30 @@ proc main =
       b = 0
       case e.kind:
       of QuitEvent: bRun = false; break
-      of MOUSEMOTION: mu.input_mousemove(ctx, e.motion.x, e.motion.y)
-      of MOUSEWHEEL: mu.input_scroll(ctx, 0, e.wheel.y * -30)
-      of TEXTINPUT: mu.input_text(ctx, cast[cstring](e.text.text.addr))
+      of MOUSEMOTION: ctx.input_mousemove(e.motion.x, e.motion.y)
+      of MOUSEWHEEL: ctx.input_scroll(0, e.wheel.y * -30)
+      of TEXTINPUT: input_text(ctx, cast[cstring](e.text.text.addr))
       of MOUSEBUTTONDOWN, MOUSEBUTTONUP:
         if button_map.hasKey(e.button.button and 0xff):
           b = button_map[e.button.button and 0xff].cint
         if (b!=0) and (e.kind == MOUSEBUTTONDOWN):
-          mu.input_mousedown(ctx, e.button.x, e.button.y, b)
+          ctx.input_mousedown(e.button.x, e.button.y, b)
         if (b!=0) and (e.kind == MOUSEBUTTONUP):
-          mu.input_mouseup(ctx, e.button.x, e.button.y, b)
+          ctx.input_mouseup(e.button.x, e.button.y, b)
       of KEYDOWN, KEYUP:
         if key_map.hasKey(e.key.keysym.sym and 0xff):
           b = key_map[e.key.keysym.sym and 0xff].cint
         if (b!=0) and (e.kind == KEYDOWN):
-          mu.input_keydown(ctx, b)
+          ctx.input_keydown(b)
         if (b!=0) and (e.kind == KEYUP):
-          mu.input_keyup(ctx, b)
+          ctx.input_keyup(b)
       else: discard
 
     process_frame(ctx)
 
     r_clear(mu.color(bg[0].cint, bg[1].cint, bg[2].cint, 255.cint))
     var cmd: ptr mu.Command = nil
-    while mu.next_command(ctx, cmd.addr) != 0:
+    while ctx.next_command(cmd.addr) != 0:
       case cmd.typec:
       of COMMAND_TEXT: r_draw_text(cast[cstring](cmd.text.str[0].addr), cmd.text.pos, cmd.text.color)
       of COMMAND_RECT: r_draw_rect(cmd.rect.rect, cmd.rect.color)
